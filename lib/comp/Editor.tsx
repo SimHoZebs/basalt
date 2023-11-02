5159fb79feb22c6c1ffd43f6c77496f6c161277c
Sim Ho <simhozebs@gmail.com>
2023-11-02 09:42:15 -0400

    Add custom node view and test node to editor

--- lib/comp/Editor.tsx
+++ lib/comp/Editor.tsx
@@ -1,16 +1,46 @@
 import React from "react";
-import { useEditor, EditorContent } from "@tiptap/react";
+import {
+  useEditor,
+  EditorContent,
+  NodeViewWrapper,
+  mergeAttributes,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import CustomStarterKit from "./CustomStaterKit";
 
 import { Node } from "@tiptap/core";
 import { ReactNodeViewRenderer } from "@tiptap/react";
-
-const Component = () => <div>Test</div>;
+const Component = () => (
+  <NodeViewWrapper>
+    <div>Helo</div>
+  </NodeViewWrapper>
+);
 
 const Test = Node.create({
   // configuration …
+  name: "test",
+  group: "block",
+  atom: true,
 
+  addAttributes() {
+    return {
+      count: {
+        default: 0,
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "test",
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ["test", mergeAttributes(HTMLAttributes)];
+  },
   addNodeView() {
     return ReactNodeViewRenderer(Component);
   },
@@ -18,9 +48,11 @@ const Test = Node.create({
 
 const Editor = () => {
   const editor = useEditor({
-    extensions: [CustomStarterKit],
-    content: "<p>Hello World!</p>",
+    extensions: [CustomStarterKit, Test],
+    content: `<p>Hello World!</p> 
+    <test></test>`,
   });
+
   return <EditorContent editor={editor} />;
 };
 
